c72cf0d519c2f6cc05dba01fa1d3ee0aa3fc0709
GabrielRSSSC <gabrielrodriguescitizen@gmail.com>
2026-09-01 18:37:47 -0300

    Projeto de luciana v1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,10 +10,10 @@ export default function Page() {
     <div>
       <Header />
       <Menu />
-      <CardAluno nome="Fernando" curso="amizade e magia" periodo="3º" />
-      <CardDisciplina nome="Honestidade" professor="Leon Kennedy" nota="9,0" />
-      <CardDisciplina nome="Lealdade" professor="Xavier" nota="8,5" />
-      <CardDisciplina nome="generosidade" professor="Rarity" nota="7,8" />
+      <CardAluno nome="João" curso="Ciência da Computação" periodo="3º" />
+      <CardDisciplina nome="Front-End Frameworks" professor="Luciana" nota="9,0" />
+      <CardDisciplina nome="Algoritmos" professor="Professor X" nota="8,5" />
+      <CardDisciplina nome="Banco de Dados" professor="Professor Y" nota="7,8" />
       <Botao texto="Entrar" />
       <Footer />
     </div>
